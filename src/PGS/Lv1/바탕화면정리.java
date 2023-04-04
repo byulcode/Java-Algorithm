56ad64a2028a2cc400c3c1fa94bd5f7f0ddd4ba3
byulcode <bybit0408@gmail.com>
2023-04-04 21:38:37 +0900

    [PGS] 바탕화면 정리(수정)

--- src/PGS/Lv1/바탕화면정리.java
+++ src/PGS/Lv1/바탕화면정리.java
@@ -1,19 +1,21 @@
 package PGS.Lv1;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/161990
-public int[] solution(String[] wallpaper) {
-    int lux = 51, luy = 51;
-    int rdx = 0, rdy = 0;
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int lux = 51, luy = 51;
+        int rdx = 0, rdy = 0;
 
-    for (int r = 0; r < wallpaper.length; r++) {
-        for (int c = 0; c < wallpaper[0].length(); c++) {
-            if (wallpaper[r].charAt(c) == '#') {
-                lux = Math.min(r, lux);
-                luy = Math.min(c, luy);
-                rdx = Math.max(r, rdx);
-                rdy = Math.max(c, rdy);
+        for (int r = 0; r < wallpaper.length; r++) {
+            for (int c = 0; c < wallpaper[0].length(); c++) {
+                if (wallpaper[r].charAt(c) == '#') {
+                    lux = Math.min(r, lux);
+                    luy = Math.min(c, luy);
+                    rdx = Math.max(r, rdx);
+                    rdy = Math.max(c, rdy);
+                }
             }
         }
+        return new int[]{lux, luy, rdx + 1, rdy + 1};
     }
-    return new int[]{lux, luy, rdx + 1, rdy + 1};
 }
