94b31afbd6cca1cf813470f70ec168416c4b3db8
byulcode <bybit0408@gmail.com>
2024-07-01 19:55:54 +0900

    [Silver V] Title: 숫자 카드, Time: 884 ms, Memory: 128216 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static int m, n;
-	static int[] result;
+	static StringBuilder sb = new StringBuilder();
 	static String[] arr;
 
 	public static void main(String[] args) throws Exception {
@@ -20,20 +20,16 @@ public class Main {
 			set.add(s);
 		}
 		m = Integer.parseInt(br.readLine());
-		result = new int[m];
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < m; i++) {
 			if (set.contains(st.nextToken())) {
-				result[i] = 1;
+				sb.append("1 ");
 			} else {
-				result[i] = 0;
+				sb.append("0 ");
 			}
 		}
-
-		for (int r : result) {
-			System.out.print(r + " ");
-		}
+		System.out.println(sb.toString());
 	}
 }
 
