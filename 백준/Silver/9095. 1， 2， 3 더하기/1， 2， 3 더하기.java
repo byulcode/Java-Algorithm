a1d05a64fab943713c714b47c2f70c2ba8ae6f36
byulcode <bybit0408@gmail.com>
2024-07-02 23:21:15 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,49 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
+// 점화식 구하기
 public class Main {
 
 	static int t;
-	static int[] arr = new int[11];
-	static List<Integer> result = new ArrayList<>();
+	static int[] dp = new int[11];
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		t = Integer.parseInt(br.readLine());
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
 
-		for (int i = 0; i < 11; i++) {
-			count(i, i);
+		for (int i = 4; i < 11; i++) {
+			dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
 		}
 
 		for (int i = 0; i < t; i++) {
 			int n = Integer.parseInt(br.readLine());
-			result.add(arr[n]);
-		}
-
-		for (int r : result) {
-			System.out.println(r);
-		}
-	}
-
-	public static void count(int k, int now) {
-		if (k == 0) {
-			arr[now]++;
-			return;
-		}
-
-		if (k - 1 >= 0) {
-			count(k - 1, now);
-		}
-
-		if (k - 2 >= 0) {
-			count(k - 2, now);
-		}
-
-		if (k - 3 >= 0) {
-			count(k - 3, now);
+			System.out.println(dp[n]);
 		}
 	}
 }
-
