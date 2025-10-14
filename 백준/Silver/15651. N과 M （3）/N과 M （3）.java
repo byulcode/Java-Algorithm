409e4a24661835cc29a4a77a34394395021381c7
byulcode <bybit0408@gmail.com>
2025-10-14 23:58:52 +0900

    [Silver III] Title: N과 M (3), Time: 488 ms, Memory: 71748 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,34 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-
 	static int n, m;
-	static int[] arr = new int[10];    // 수열을 저장할 배열
-	static int[] isChecked = new int[10]; // 해당 숫자가 수열에 포함되었는지 체크 여부
+	static int[] arr;
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		n = scanner.nextInt();
 		m = scanner.nextInt();
+		arr = new int[m];
 
-		printNums(0);
+		func(0);
 		System.out.println(sb.toString());
 	}
 
-	// 현재 k개까지 수를 택한 상황에서 arr[k]를 정하는 함수
-	static void printNums(int k) {
+	static void func(int k) {
 		if (k == m) {
-			for (int i = 0; i < m; i++) {
-				sb.append(arr[i]).append(" ");
+			for (int a : arr) {
+				sb.append(a).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
+
 		for (int i = 1; i <= n; i++) {
 			arr[k] = i;
-			printNums(k + 1);    // 중복을 허용함
+			func(k + 1);
 		}
 	}
 }
-
